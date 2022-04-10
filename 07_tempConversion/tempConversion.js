e6c99f47beda0ffd77a4c8d7e019aa7a2b590915
Conor Dunne <conordunne@outlook.ie>
2022-04-10 17:19:32 +0900

    Add rest of solutions for Fundamentals Part 4 (to tempConversion)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,33 @@
-const ftoc = function() {
+const ftoc = function(f) {
+
+let result = ((f - 32) * 5/9).toFixed(1);
+
+if (result == 0) return(0);
+else return Number(result);
 
 };
 
-const ctof = function() {
+
+
+const ctof = function(c) {
+
+  let result = (c * 9/5 + 32).toFixed(1);
+
+  if (result == 0) return(0);
+  else return Number(result);
+
 
 };
+
+
+console.log(ctof(73.2));
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
 };
+
+
+//ftoc (1034°F − 32) × 5/9 = 556.667°C
+//ctof  [°F] = [°C] × 9⁄5 + 32
