87c760bc290f2dfba4647fa44ef4adee1c1343cd
cdutd83 <cdutd83@gmail.comasd>
2022-05-13 06:54:33 +0900

    Add my solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const forwards = string.toUpperCase().split(" ").join("").replace(/[^a-zA-Z]/g,"");
+    const backwards = forwards.split("").reverse().join("");
+    return forwards === backwards;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    const userIndex = parseInt(num) - 1;
+    let arr = [1, 1, 2, 3, 5, 8];
+    let lastIndex = arr.length - 1;
+
+    if (userIndex < 0) {
+        return "OOPS";
+    }
+    else if (lastIndex >= userIndex) {
+        return arr[userIndex];
+    }
+    else {
+        while (lastIndex < userIndex) {
+            arr.push(arr[lastIndex] + arr[lastIndex - 1]);
+            lastIndex = arr.length - 1;
+        }
+        return arr[userIndex];
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
